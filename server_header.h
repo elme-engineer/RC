e03a0d990218fffb9294ee6d501115eb22cf99cf
elme-engineer <pmcb.uc@gmail.com>
2024-05-12 06:03:52 +0100

    Still a lot to do

--- server_header.h
+++ server_header.h
@@ -21,23 +21,37 @@
 
 int tcp_client_fd, udp_port, tcp_port;
 
+int users_amount;
 
+char *filename;
 
 typedef struct user{
 	int socketfd;
 	char *username;
 	char *password;
 	char *type;
-}User;
+	struct user *next;
+	struct user *prev;
+	
+
+}User, *pUser;
 
 User* users_array;
 
+pUser user_list_head;
+pUser user_list_tail;
+
 void error(char *str);
 void init(void);
-void read_config_file(char *file_name);
+void read_config_file();
+void save_config_file();
+void free_node(pUser node);
 void *udp();
 void *tcp();
 void process_client(int client_fd);
+int validate_user(char *username);
+void add_user(pUser new_user);
+int del_user(char *username);
 int get_user(char *username, char *password);
 void send_msg_udp(char *msg, int udp_fd, socklen_t cliente_socket_len, struct sockaddr_in client_addr);
 
